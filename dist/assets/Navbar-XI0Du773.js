52e0d27ad0e998141461f9916d9a4cfdd565151b
Moh sahal <mohammedsahal1243@gmail.com>
2026-09-06 22:11:30 +0530

    fix(work): perfect screenshot alignment, 16:9 aspect ratio and seamless container fit

--- dist/assets/Navbar-XI0Du773.js
+++ dist/assets/Navbar-XI0Du773.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-CImyIhIF.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-D7gqAM1Q.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
