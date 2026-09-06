803958f31f244f28e9c36d168539eaa3658101ae
Moh sahal <mohammedsahal1243@gmail.com>
2026-09-06 22:07:27 +0530

    fix: resolve mobile layout issues, carousel slide widths, frosted glass navbar, and social icon positioning

--- dist/assets/Navbar-fpd3nSd8.js
+++ dist/assets/Navbar-fpd3nSd8.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-BQnpn59v.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-CImyIhIF.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
